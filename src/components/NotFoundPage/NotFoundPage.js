b30d7d9763568cbacf5c90d686bc467e0a4e93fe
Dmitrii Liadov <d.liadov@xsolla.com>
2023-08-15 18:05:49 +0400

    refactor: add handleGoBack function

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -3,16 +3,15 @@ import { useNavigate } from 'react-router-dom';
 function NotFoundPage() {
   const navigate = useNavigate();
 
+  function handleGoBack() {
+    navigate(-1);
+  }
+
   return (
     <div className="not-found-page">
       <h2 className="not-found-page__title">404</h2>
       <p className="not-found-page__text">Страница не найдена</p>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-        className="not-found-page__back-button"
-      >
+      <button onClick={handleGoBack} className="not-found-page__back-button">
         Назад
       </button>
     </div>
